b8e09355e916799399abbe7f62e3460fb9a4864c
gertrukas <gerardo.trujillo@gmail.com>
2024-04-16 11:52:31 -0600

    fecha formateo

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
+import { registerLocaleData } from '@angular/common';
+
+import localePy from '@angular/common/locales/es-MX';
+
+registerLocaleData(localePy, 'es');
 
 @NgModule({
   declarations: [
@@ -16,7 +21,7 @@ import { HttpClientModule } from "@angular/common/http";
     BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'es-Mx' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
