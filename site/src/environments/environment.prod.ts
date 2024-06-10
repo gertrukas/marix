b2eee8ecb59d1a86154a5960a1076abde356c611
Kepler Reyes Hernandez <100866889+Kepler29@users.noreply.github.com>
2024-06-10 13:23:40 -0600

    environments

--- site/src/environments/environment.prod.ts
+++ site/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   appVersion: 'v1.0.1',
   USERDATA_KEY: 'authexcess2024',
-  apiUrl: 'https://backend.matrix.com/api',
-  adminUrl: 'https://admin.matrix.com/',
-  pageUrl: 'https://matrix.com/',
+  apiUrl: 'https://backend.xsbeta.com/api',
+  adminUrl: 'https://admin.xsbeta.com/',
+  pageUrl: 'https://xsbeta.com/',
 };
